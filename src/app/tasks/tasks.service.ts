ebbc0a40e6b1ce4536a4c5064b8f0dd08c6eb65e
Nishee <nishee.work05@gmail.com>
2026-07-09 10:52:32 +0530

    DAY4 Getting strarted with services

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { type NewTaskData } from "./task/task.model";
+
+class TasksService  {
+    private tasks = [
+        {
+          id: 't1',
+          userId: 'u1',
+          title: 'MASTER ANGULAR',
+          summary: 'Learn all the basic and advanced features of angular and how to apply them.',
+          dueDate: '2025-12-31'
+        },
+        {
+          id: 't2',
+          userId: 'u3',
+          title: 'BUILD FIRST PROTOTYPE',
+          summary: 'Build first prototype of the online shop website.',
+          dueDate: '2025-05-31'
+        },    
+        {
+          id: 't3',
+          userId: 'u3',
+          title: '  PREPARE ISSUE TEMPLATE',
+          summary: 'Prepare and describe an issue template which will help with project management',
+          dueDate: '2024-06-15'
+        },
+    ];
+
+    getUSerTasks(userId: string) {
+        return this.tasks.filter((task)=>task.userId === this.userId );
+    }
+
+    addTask (taskData: NewTaskData, userId: string) {
+        this.tasks.push({
+            id: new Date().getTime().toString(),
+            userId: userId,
+            title: taskData.title,
+            summary: taskData.summary,
+            dueDate: taskData.date
+        }) 
+    }
+    removeTask(id:string){
+        this.tasks = this.tasks.filter((task)=> task.id !== id);
+    }    
+}
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -16,35 +16,12 @@ export class Tasks {
 
   isAddingTask : boolean = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'MASTER ANGULAR',
-      summary: 'Learn all the basic and advanced features of angular and how to apply them.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'BUILD FIRST PROTOTYPE',
-      summary: 'Build first prototype of the online shop website.',
-      dueDate: '2025-05-31'
-    },    
-    {
-      id: 't3',
-      userId: 'u3',
-      title: '  PREPARE ISSUE TEMPLATE',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15'
-    },
-  ];
 
   get selectedUserTasks(){
-    return this.tasks.filter((task)=>task.userId === this.userId );
+    return
   }
   onCompleteTask(id:string) {
-    this.tasks = this.tasks.filter((task)=> task.id !== id);
+    
   }
   onStartAddTask() {
     this.isAddingTask = true
@@ -53,13 +30,7 @@ export class Tasks {
     this.isAddingTask = false
   }
   onAddTask(taskData: NewTaskData){
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    } ) 
+
     this.isAddingTask = false;
   }
 }
