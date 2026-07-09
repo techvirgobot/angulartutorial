97567eca2e6222d159c6f5ac3a1f4e708d0debfe
Nishee <nishee.work05@gmail.com>
2026-07-09 10:29:28 +0530

    DAY$ Transforming Date data into pipes

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskObject } from './task.model';
 import { Card } from "../../shared/card/card";
+import  {DatePipe} from "@angular/common"
 
 @Component({
   standalone: true,
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
